59891879df7d1d7458612dad551e37b6d1500046
hoffrenm <46321023+hoffrenm@users.noreply.github.com>
2023-02-21 13:38:24 +0200

    Working database connection

--- src/main/java/org/example/Repositories/CustomerRepository.java
+++ src/main/java/org/example/Repositories/CustomerRepository.java
@@ -1,6 +1,25 @@
 package org.example.Repositories;
 
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Component
 public class CustomerRepository extends Generic {
+
+    private String url = "jdbc:postgresql://localhost:5432/chinook";
+    private String username = "postgres";
+    private String password = "postgres";
+
+    public void test() {
+        try(Connection conn = DriverManager.getConnection(url, username,password);) {
+            System.out.println("Connected to Postgres...");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     //opening a database connection etc?
     //jdbc template?
