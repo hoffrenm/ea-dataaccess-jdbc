d1ffce6a6f744849d2ee82ed53acbc45d992a24e
hoffrenm <46321023+hoffrenm@users.noreply.github.com>
2023-02-21 17:34:44 +0200

    Create SQL queries with JDBC
    Queries for findById, paged customers, update and highest spender.

--- src/main/java/org/example/Models/Customer.java
+++ src/main/java/org/example/Models/Customer.java
@@ -1,11 +1,90 @@
 package org.example.Models;
 
 public class Customer {
-    private Integer customer_id;
-    private String first_name;
-    private String last_name;
+    private int id;
+    private String firstName;
+    private String lastName;
     private String country;
-    private String postal_code;
+    private String postalCode;
     private String phone;
     private String email;
+
+    public Customer(int customerId, String firstName, String lastName, String country, String postalCode, String phone, String email) {
+        this.id = customerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+        this.postalCode = postalCode;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.id = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", country='" + country + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
--- src/main/java/org/example/Repositories/CustomerRepository.java
+++ src/main/java/org/example/Repositories/CustomerRepository.java
@@ -1,13 +1,15 @@
 package org.example.Repositories;
 
+import org.example.Models.Customer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-@Component
+@Repository
 public class CustomerRepository extends Generic {
 
     private final String url;
@@ -24,20 +26,118 @@ public class CustomerRepository extends Generic {
     }
 
     public void test() {
-        try(Connection conn = DriverManager.getConnection(url, username,password);) {
+        try (Connection conn = DriverManager.getConnection(url, username, password);) {
             System.out.println("Connected to Postgres...");
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    //opening a database connection etc?
-    //jdbc template?
+    // Assignment part 2.2
+    public Customer findById(int id) {
+        String sql = "SELECT * FROM customer WHERE customer_id = ?";
+        Customer customer = null;
 
-    //1. Find all customers
+        try (Connection conn = DriverManager.getConnection(url, username, password)) {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setInt(1, id);
+            ResultSet result = statement.executeQuery();
 
+            while (result.next()) {
+                customer = new Customer(
+                        result.getInt("customer_id"),
+                        result.getString("first_name"),
+                        result.getString("last_name"),
+                        result.getString("country"),
+                        result.getString("postal_code"),
+                        result.getString("phone"),
+                        result.getString("email")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-    //2. Find a specific customer
+        return customer;
+    }
 
-    //etc!
+    // Assignment part 2.4
+    public List<Customer> customerPage(int limit, int pageNum) {
+        int offset = limit * pageNum;
+        String sql = "SELECT * FROM customer LIMIT ? OFFSET ?";
+        List<Customer> customerPage = new ArrayList<>();
+
+        try (Connection conn = DriverManager.getConnection(url, username, password)) {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setInt(1, limit);
+            statement.setInt(2, offset);
+            ResultSet result = statement.executeQuery();
+
+            while (result.next()) {
+                customerPage.add(
+                        new Customer(
+                                result.getInt("customer_id"),
+                                result.getString("first_name"),
+                                result.getString("last_name"),
+                                result.getString("country"),
+                                result.getString("postal_code"),
+                                result.getString("phone"),
+                                result.getString("email")
+                        ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return customerPage;
+    }
+
+    // Assignment part 2.6
+    public int update(Customer customer) {
+        String sql = "UPDATE customer SET first_name = ?, last_name = ?, country = ?, postal_code = ?, phone = ?, email = ? WHERE customer_id = ?";
+        int result = 0;
+
+        try (Connection conn = DriverManager.getConnection(url, username, password)) {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, customer.getFirstName());
+            statement.setString(2, customer.getLastName());
+            statement.setString(3, customer.getCountry());
+            statement.setString(4, customer.getPostalCode());
+            statement.setString(5, customer.getPhone());
+            statement.setString(6, customer.getEmail());
+            statement.setInt(7, customer.getId());
+
+            result = statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    // Assignment part 2.8
+    public Customer highestSpender() {
+        String sql = "SELECT * FROM customer WHERE customer_id = (SELECT customer_id FROM invoice GROUP BY customer_id ORDER BY SUM(invoice.total) DESC LIMIT 1)";
+        Customer customer = null;
+
+        try (Connection conn = DriverManager.getConnection(url, username, password)) {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            ResultSet result = statement.executeQuery();
+
+            while (result.next()) {
+                customer = new Customer(
+                        result.getInt("customer_id"),
+                        result.getString("first_name"),
+                        result.getString("last_name"),
+                        result.getString("country"),
+                        result.getString("postal_code"),
+                        result.getString("phone"),
+                        result.getString("email")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return customer;
+    }
 }
