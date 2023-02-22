f3523ed2292fd1cb17d5bd862656ffb597870cf3
hoffrenm <46321023+hoffrenm@users.noreply.github.com>
2023-02-22 15:11:48 +0200

    Add CustomerSpender, modify method, add comments

---
+++ src/main/java/org/example/Models/CustomerSpender.java
@@ -0,0 +1,4 @@
+package org.example.Models;
+
+public record CustomerSpender(int id, String firstName, String lastName, double total) {
+}
--- src/main/java/org/example/Repositories/CustomerRepositoryImpl.java
+++ src/main/java/org/example/Repositories/CustomerRepositoryImpl.java
@@ -3,6 +3,7 @@ package org.example.Repositories;
 import org.example.Models.Customer;
 import org.example.Models.CustomerCountry;
 import org.example.Models.CustomerGenre;
+import org.example.Models.CustomerSpender;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,10 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Implementation of CustomerRepository that handles database operations associated
+ * with Customer class.
+ */
 @Repository
 public class CustomerRepositoryImpl implements CustomerRepository {
 
@@ -17,6 +22,14 @@ public class CustomerRepositoryImpl implements CustomerRepository {
     private final String username;
     private final String password;
 
+    /**
+     * Creates a new CustomerRepository with given database name,
+     * username and password.
+     *
+     * @param url url of database
+     * @param username database user username
+     * @param password database user password
+     */
     public CustomerRepositoryImpl(
             @Value("${spring.datasource.url}") String url,
             @Value("${spring.datasource.username}") String username,
@@ -26,7 +39,14 @@ public class CustomerRepositoryImpl implements CustomerRepository {
         this.password = password;
     }
 
-    // Method for creating a customer out of ResultSet
+    /**
+     * Creates a new Customer object from ResultSet. By default, contains
+     * id, first name, last name, county, postal code, phone and email.
+     *
+     * @param result ResultSet object that is returned by SQL query
+     * @return Customer object
+     * @throws SQLException
+     */
     private static Customer createCustomerFromResult(ResultSet result) throws SQLException {
         Customer customer = new Customer(
                 result.getInt("customer_id"),
@@ -43,13 +63,18 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 
     // Assignment part 2.1 Read all the customers in the database
 
+    /**
+     * Returns all customers from the database.
+     *
+     * @return list of all customers
+     */
     @Override
     public List<Customer> findAll() {
 
         List<Customer> customers = new ArrayList<>();
 
         try (Connection conn = DriverManager.getConnection(url, username, password)) {
-            PreparedStatement statement = conn.prepareStatement("SELECT customer_id, first_name, last_name, country, postal_code, phone, email FROM CUSTOMER");
+            PreparedStatement statement = conn.prepareStatement("SELECT customer_id, first_name, last_name, country, postal_code, phone, email FROM customer");
             ResultSet result = statement.executeQuery();
 
             while (result.next()) {
@@ -65,9 +90,16 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 
 
     // Assignment part 2.2 Read a specific customer from the database (by Id)
+
+    /**
+     * Reads a specific customer from the database identified by unique id
+     *
+     * @param id unique id
+     * @return Customer object if id exists, otherwise null
+     */
     @Override
     public Customer findById(Integer id) {
-        String sql = "SELECT * FROM customer WHERE customer_id = ?";
+        String sql = "SELECT customer_id, first_name, last_name, country, postal_code, phone, email FROM customer WHERE customer_id = ?";
         Customer customer = null;
 
         try (Connection conn = DriverManager.getConnection(url, username, password)) {
@@ -94,6 +126,14 @@ public class CustomerRepositoryImpl implements CustomerRepository {
     }
 
     // Assignment part 2.3 Read a specific customer by name
+
+    /**
+     * Reads a customer from database by trying to match
+     * input string to first name or last name.
+     *
+     * @param name string to match
+     * @return Customer object if name matches, otherwise null
+     */
     public Customer findByName(String name) {
         Customer customer = null;
 
@@ -115,9 +155,18 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 
     // Assignment part 2.4 Return a page of customers from the database
 
+    /**
+     * Reads a page of customers from database. First parameter indicates number
+     * of results and second parameter indicates page number.
+     * Offsets query by limit * pageNum.
+     *
+     * @param limit number of customers
+     * @param pageNum page number
+     * @return list of customers on certain page
+     */
     public List<Customer> customerPage(int limit, int pageNum) {
         int offset = limit * pageNum;
-        String sql = "SELECT * FROM customer LIMIT ? OFFSET ?";
+        String sql = "SELECT customer_id, first_name, last_name, country, postal_code, phone, email FROM customer LIMIT ? OFFSET ?";
         List<Customer> customerPage = new ArrayList<>();
 
         try (Connection conn = DriverManager.getConnection(url, username, password)) {
@@ -147,11 +196,17 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 
     //Assignment part 2.5 Add a new customer to the database
 
-    public int insert(Customer customer){
+    /**
+     * Inserts a new customer to database.
+     *
+     * @param customer Customer object
+     * @return 1 for successful operation, 0 otherwise
+     */
+    public int insert(Customer customer) {
         int rowsAffected = 0;
         try (Connection conn = DriverManager.getConnection(url, username, password)) {
 
-            PreparedStatement statement = conn.prepareStatement("INSERT INTO customer ( first_name, last_name, country, postal_code, phone, email) VALUES (?, ?, ?, ?, ?, ?)");
+            PreparedStatement statement = conn.prepareStatement("INSERT INTO customer (first_name, last_name, country, postal_code, phone, email) VALUES (?, ?, ?, ?, ?, ?)");
             statement.setString(1, customer.getFirstName());
             statement.setString(2, customer.getLastName());
             statement.setString(3, customer.getCountry());
@@ -174,7 +229,6 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 
 
     // Assignment part 2.6 Update an existing customer
-
     public int update(Customer customer) {
         String sql = "UPDATE customer SET first_name = ?, last_name = ?, country = ?, postal_code = ?, phone = ?, email = ? WHERE customer_id = ?";
         int result = 0;
@@ -197,7 +251,6 @@ public class CustomerRepositoryImpl implements CustomerRepository {
     }
 
     // Assignment part 2.7 Return the country with the most customers
-
     public CustomerCountry countryWithMostCustomers() throws SQLException {
 
         try (Connection conn = DriverManager.getConnection(url, username, password)) {
@@ -215,24 +268,20 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 
 
     // Assignment part 2.8 Return the customer who is the highest spender
-
-    public Customer highestSpender() {
-        String sql = "SELECT * FROM customer WHERE customer_id = (SELECT customer_id FROM invoice GROUP BY customer_id ORDER BY SUM(invoice.total) DESC LIMIT 1)";
-        Customer customer = null;
+    public CustomerSpender highestSpender() {
+        String sql = "SELECT customer.customer_id, customer.first_name, customer.last_name, SUM(invoice.total) as total FROM customer JOIN invoice ON customer.customer_id=invoice.customer_id GROUP BY customer.customer_id ORDER BY total DESC LIMIT 1";
+        CustomerSpender customer = null;
 
         try (Connection conn = DriverManager.getConnection(url, username, password)) {
             PreparedStatement statement = conn.prepareStatement(sql);
             ResultSet result = statement.executeQuery();
 
             while (result.next()) {
-                customer = new Customer(
+                customer = new CustomerSpender(
                         result.getInt("customer_id"),
                         result.getString("first_name"),
                         result.getString("last_name"),
-                        result.getString("country"),
-                        result.getString("postal_code"),
-                        result.getString("phone"),
-                        result.getString("email")
+                        result.getDouble("total")
                 );
             }
         } catch (SQLException e) {
